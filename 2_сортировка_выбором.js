8105afe6fe03d4affaf421f08d720729f654d9e3
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-10-11 19:00:57 +0300

    стр 65 Грокаем алгоритмы

---
+++ 2_сортировка_выбором.js
@@ -0,0 +1,68 @@
+'use strict';
+// Selection Sort - O(n^2)
+// Parameter:
+//  1. random array
+
+function findSmallest(arr) {
+	let smallest = arr[0];
+	let smallest_index = 0;
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] < smallest) {
+			smallest = arr[i];
+			smallest_index = i;
+		}
+	}
+	return smallest_index; //* возвращает индекс наименьшего  элемента в массиве
+}
+
+const ar = [1, 5, 4, 8, 2, 8, 9, 0, 6, 4, 2]
+
+// console.log(findSmallest())
+
+//* АЛГОРИТМ СОРТИРОВКИ ВЫБОРОМ
+
+function selectionSort(arr) {
+	function findSmallest(arr) {
+		let smallest = arr[0];
+		let smallest_index = 0;
+		for (let i = 1; i < arr.length; i++) {
+			if (arr[i] < smallest) {
+				smallest = arr[i];
+				smallest_index = i;
+			}
+		}
+		return smallest_index; //* возвращает индекс наименьшего  элемента в массиве
+	}
+	let arrCopie = [...arr];
+	const sortedArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		let smallest = findSmallest(arrCopie);
+		sortedArr.push(arrCopie.splice(smallest, 1)[0]); //? [0] нужен для
+																	//? разворачивания массива возвращаемого splice
+	}
+	return sortedArr;
+}
+
+console.log('no recursion', selectionSort(ar))
+
+//* АЛГОРИТМ СОРТИРОВКИ ВЫБОРОМ ЧЕРЕЗ РЕКУРСИЮ
+
+function selectionSortRecursion(arr) {
+	function findSmallest(arr) {
+		let smallest = arr[0];
+		let smallest_index = 0;
+		for (let i = 1; i < arr.length; i++) {
+			if (arr[i] < smallest) {
+				smallest = arr[i];
+				smallest_index = i;
+			}
+		}
+		return smallest_index; //* возвращает индекс наименьшего  элемента в массиве
+	}
+	let arrCopie = [...arr]; //* необходимо для избежания модификации списка из аргументов ( arr )
+	if (!arrCopie.length) return []; //? выход из рекурсии при завершении списка
+	let smallest = arrCopie.splice(findSmallest(arrCopie), 1);
+	return smallest.concat(selectionSort(arrCopie)); //? передаваемый в рекурсивную функцию список короче на 1 элемент
+}
+
+console.log('recursion',selectionSortRecursion(ar));
